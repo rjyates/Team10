37f494349dbc9d6538ca0af5884500d94855ab44
maddie-wilkinson <144154893+maddie-wilkinson@users.noreply.github.com>
2023-09-26 14:02:56 -0500

    Add files via upload

---
+++ PlayerEntry.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+// Need to change JLabel to a drawImage() so that it properly fits the screen
+/* questions while coding:
+- is it bad to have different Jframe classes for the loading screen, player entry, and actual game?
+- should I do two seperate classes (the frame one and just player entry) or just one? - bc im thinking one and it shouldn't be bad to merge them once I get down the logic
+*/
+class PlayerEntry {
+	public static void main(String[] args) {
+		ImageIcon backround = new ImageIcon("PlayerEntryScreen.png");
+		JLabel myLabel = new JLabel(backround);
+		myLabel.setSize(3487, 2221);
+		
+		PlayerEntryFrame f = new PlayerEntryFrame(4000, 3000);
+		f.add(myLabel);
+		
+		
+	}
+}
---
+++ PlayerEntryFrame.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+
+public class PlayerEntryFrame extends JFrame {
+	private int width;
+	private int height;
+	
+	public PlayerEntryFrame(int w, int h){
+		width = w;
+		height = h;
+		setSize(width, height);
+		setTitle("Player Entry Screen");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+	public static void main(String[] args){
+		
+	}
+}
