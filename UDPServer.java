afcca687010ab80cc94cdfbbcceef6eac98f72cf
Regan Yates <75269018+rjyates@users.noreply.github.com>
2023-09-29 22:55:41 -0500

    Update UDPServer.java

--- UDPServer.java
+++ UDPServer.java
@@ -3,63 +3,52 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
-import java.util.ArrayList;
 
-public class UDPServer implements Runnable {
-	public ArrayList<Integer> Shoot = new ArrayList<>();
-	public ArrayList<Integer> Hit = new ArrayList<>();
-	DatagramSocket rec, brd;
-
-	public void run() {
-		try {
-			rec = new DatagramSocket(7501);
-			brd = new DatagramSocket(7500);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}	
-
+public class UDPServer
+{
+	public static void main(String[] args) throws IOException
+	{
+		// Step 1 : Create a socket to listen at port 1234
+		DatagramSocket ds = new DatagramSocket(7501);
 		byte[] receive = new byte[65535];
-		DatagramPacket DpReceive = null;
 
-		while (true) {
+		DatagramPacket DpReceive = null;
+		while (true)
+		{
+
+			// Step 2 : create a DatgramPacket to receive the data.
 			DpReceive = new DatagramPacket(receive, receive.length);
 
-			try {
-				rec.receive(DpReceive);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+			// Step 3 : revieve the data in byte buffer.
+			ds.receive(DpReceive);
 
 			System.out.println("Client:-" + data(receive));
-			String message = data(receive).toString();
-			String[] playerIds = message.split("\\:");
-			int idOfPlayerShooting = Integer.parseInt(playerIds[0]);
-			int idOfPlayerHit = Integer.parseInt(playerIds[1]);
-			Shoot.add(idOfPlayerShooting);
-			Hit.add(idOfPlayerHit);
-			try {
-				// Broadcast player hit
-				brd.send(
-					new DatagramPacket(
-						new byte[]{(byte) idOfPlayerHit},
-						1));
-			} catch (IOException e) {
-				e.printStackTrace();
-			}	
+
+			// Exit the server if the client sends "bye"
+			if (data(receive).toString().equals("bye"))
+			{
+				System.out.println("Client sent bye.....EXITING");
+				break;
+			}
+
+			// Clear the buffer after every message.
 			receive = new byte[65535];
 		}
 	}
 
-	public static StringBuilder data(byte[] a) {
+	// A utility method to convert the byte array
+	// data into a string representation.
+	public static StringBuilder data(byte[] a)
+	{
 		if (a == null)
 			return null;
 		StringBuilder ret = new StringBuilder();
 		int i = 0;
-		while (a[i] != 0) {
+		while (a[i] != 0)
+		{
 			ret.append((char) a[i]);
 			i++;
 		}
 		return ret;
 	}
-
 }
