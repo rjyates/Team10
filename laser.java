de1845e9924a29b7fe4df87d7c24dc35421f0101
charliewhitehead <144290176+charliewhitehead@users.noreply.github.com>
2023-10-01 19:29:33 -0500

    Updated laser.java

--- laser.java
+++ laser.java
@@ -9,6 +9,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 public class laser {
     private JFrame frame;
@@ -94,35 +95,42 @@ public class laser {
         blue.setBackground(Color.BLACK);
         blue.setOpaque(true);
         blue.setBorderPainted(false);
-        blue.setBounds(500, 5, 200, 50);
+        blue.setBounds(650, 5, 200, 50);
         blue.setFont(new Font("Georgia", Font.PLAIN, 30));
         f.add(blue);
 
         // Create an array to hold the pink button locations
-        int[][] buttonLocationsP = {
+        int[][] buttonLocationsPid = {
                 { 95, 73 }, { 95, 150 }, { 95, 230 }, { 95, 308 }, { 95, 388 }, { 95, 467 }, { 95, 544 },
-                { 310, 73 }, { 310, 150 }, { 310, 230 }, { 310, 308 }, { 310, 388 }, { 310, 467 }, { 310, 547 }
+        };
+        int[][] buttonLocationsPname = {
+               { 310, 73 }, { 310, 150 }, { 310, 230 }, { 310, 308 }, { 310, 388 }, { 310, 467 }, { 310, 547 }
         };
 
         // Create an array to hold the buttons
-        JButton[] buttonsP = new JButton[buttonLocationsP.length];
+        JButton[] buttonsPid = new JButton[buttonLocationsPid.length];        
+        JButton[] buttonsPname = new JButton[buttonLocationsPname.length];
 
         // Create an array to hold the blue button locations
-        int[][] buttonLocationsB = {
+        int[][] buttonLocationsBid = {
                 { 575, 73 }, { 575, 150 }, { 575, 230 }, { 575, 308 }, { 575, 388 }, { 575, 467 }, { 575, 544 },
+        };
+        int[][] buttonLocationsBname = {
                 { 783, 73 }, { 783, 150 }, { 783, 230 }, { 783, 308 }, { 783, 388 }, { 783, 467 }, { 783, 547 }
         };
 
-        JButton[] buttonsB = new JButton[buttonLocationsB.length];
+        JButton[] buttonsBid = new JButton[buttonLocationsBid.length];
+        JButton[] buttonsBname = new JButton[buttonLocationsBname.length];
 
-        for (int i = 0; i < buttonLocationsP.length; i++) {
-            int[] location = buttonLocationsP[i];
-            JButton button = new JButton("Click me to enter text");
+        //pink buttons
+        for (int i = 0; i < buttonLocationsPid.length; i++) {
+            int[] location = buttonLocationsPid[i];
+            JButton button = new JButton("Click to enter ID");
             button.setBounds(location[0], location[1], 200, 72);
             // button.setBackground(Color.BLACK);
             button.setBackground(Color.MAGENTA);
             button.setOpaque(true);
-            button.setBorderPainted(false);
+            button.setBorderPainted(true);
             button.setFont(new Font("Georgia", Font.PLAIN, 15));
             final int buttonIndex = i; // Use a final variable for ActionListener
             button.addActionListener(new ActionListener() {
@@ -131,32 +139,64 @@ public class laser {
                     String input =  JOptionPane.showInputDialog(f, "Enter value:");
                     // String name = JOptionPane.showInputDialog(f, "Enter text:");
                     if (input != null && !input.isEmpty()) {
-                        if(buttonIndex<7){ //playerID columns
-                            while (input != null && !input.matches("-?\\d+")) {
-                                input = JOptionPane.showInputDialog(f, "Enter value:");
-                            }
-                            buttonsP[buttonIndex].setText(input);
-                            handleID(input, buttonIndex, buttonsP);
+                        //playerID columns
+                        while (input != null && !input.matches("-?\\d+")) {
+                            input = JOptionPane.showInputDialog(f, "Enter value:");
                         }
-                        else{ //codeName column
-                            handleCodeName(input, buttonIndex, buttonsP);
-                            buttonsP[buttonIndex].setText(input);
-                        }
+
+                        buttonsPid[buttonIndex].setText(input);
+                        handleID(input, buttonIndex, buttonsPname);
                     }
                 }
             });
-            buttonsP[i] = button; // Store the button in the array
+            
+
+            buttonsPid[i] = button; // Store the button in the array
             f.add(button);
         }
 
-        for (int i = 0; i < buttonLocationsB.length; i++) {
-            int[] location = buttonLocationsB[i];
-            JButton button = new JButton("Click me to enter text");
+        for (int i = 0; i < buttonLocationsPname.length; i++) {
+            int[] location = buttonLocationsPname[i];
+            JButton button = new JButton("Click to enter name");
+            button.setBounds(location[0], location[1], 200, 72);
+            // button.setBackground(Color.BLACK);
+            button.setBackground(Color.MAGENTA);
+            button.setOpaque(true);
+            button.setBorderPainted(true);
+            button.setFont(new Font("Georgia", Font.PLAIN, 15));
+            final int buttonIndex = i; // Use a final variable for ActionListener
+            button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    String input =  JOptionPane.showInputDialog(f, "Enter name:");
+                    // String name = JOptionPane.showInputDialog(f, "Enter text:");
+                    if (input != null && !input.isEmpty()) {
+                        //codeName column
+                        while (input != null && !input.matches("-?\\D+")) 
+                        {
+                            input = JOptionPane.showInputDialog(f, "Enter name:");
+                        }
+                        buttonsPname[buttonIndex].setText(input);
+                        handleCodeName(input, buttonIndex, buttonsPid);
+                        
+                    }
+                }
+            });
+
+            buttonsPname[i] = button; // Store the button in the array
+            f.add(button);
+        }
+
+
+        //blue team
+        for (int i = 0; i < buttonLocationsBid.length; i++) {
+            int[] location = buttonLocationsBid[i];
+            JButton button = new JButton("Click to enter ID");
             button.setBounds(location[0], location[1], 200, 72);
             // button.setBackground(Color.BLACK);
             button.setBackground(Color.BLUE);
             button.setOpaque(true);
-            button.setBorderPainted(false);
+            button.setBorderPainted(true);
             button.setFont(new Font("Georgia", Font.PLAIN, 15));
             final int buttonIndex = i; // Use a final variable for ActionListener
             button.addActionListener(new ActionListener() {
@@ -164,37 +204,68 @@ public class laser {
                 public void actionPerformed(ActionEvent e) {
                     String input =  JOptionPane.showInputDialog(f, "Enter value:");
                     if (input != null && !input.isEmpty()) {
-                        if(buttonIndex<7){
-                            while (input != null && !input.matches("-?\\d+")) {
-                                input = JOptionPane.showInputDialog(f, "Enter value:");
-                            }
-                            buttonsB[buttonIndex].setText(input);
-                            handleID(input, buttonIndex, buttonsB);
+                        while (input != null && !input.matches("-?\\d+")) {
+                            input = JOptionPane.showInputDialog(f, "Enter value:");
+                        }
+                        buttonsBid[buttonIndex].setText(input);
+                        handleID(input, buttonIndex, buttonsBname);
                     }
-                    else{
-                        handleCodeName(input, buttonIndex, buttonsB);
-                        buttonsB[buttonIndex].setText(input);
-                    }
-                }
                 }
             });
 
-            buttonsB[i] = button; // Store the button in the array
+            buttonsBid[i] = button; // Store the button in the array
+            f.add(button);
+        }
+
+        for (int i = 0; i < buttonLocationsBname.length; i++) {
+            int[] location = buttonLocationsBname[i];
+            JButton button = new JButton("Click to enter name");
+            button.setBounds(location[0], location[1], 200, 72);
+            // button.setBackground(Color.BLACK);
+            button.setBackground(Color.BLUE);
+            button.setOpaque(true);
+            button.setBorderPainted(true);
+            button.setFont(new Font("Georgia", Font.PLAIN, 15));
+            final int buttonIndex = i; // Use a final variable for ActionListener
+            button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    String input =  JOptionPane.showInputDialog(f, "Enter name:");
+
+                    //String input =  JOptionPane.showInputDialog(f, "Enter value:");
+                    if (input != null && !input.isEmpty()) {
+                        while (input != null && !input.matches("-?\\D+")) 
+                        {
+                            input = JOptionPane.showInputDialog(f, "Enter name:");
+                        }
+                        //buttonsBid[buttonIndex].setText(input);
+                        //handleID(input, buttonIndex, buttonsBname);
+                        buttonsBname[buttonIndex].setText(input);
+                        handleCodeName(input, buttonIndex, buttonsBid);
+                    }
+                    
+                    //buttonsBname[buttonIndex].setText(input);
+                }
+            });
+
+            buttonsBname[i] = button; // Store the button in the array
             f.add(button);
         }
 
         f.setVisible(true);
     }
-    public static void handleID(String name, final int buttonIndex, JButton[] buttonsArray){
+
+
+    public static void handleID(String id, final int buttonIndex, JButton[] buttonsArray){
         try{
-            int playerID = Integer.parseInt(name); // Convert 'name' to an integer
+            int playerID = Integer.parseInt(id); // Convert 'name' to an integer
             // Call the findPlayer method of databaseHandler here and pass 'playerID' as an argument
             try {
             String codeName = databaseHandler.findPlayer(playerID);
             if (codeName != null) {
-                System.out.println("Blue Team Button " + buttonIndex + " Text: " + name);
+                System.out.println("Blue Team Button " + buttonIndex + " Text: " + id);
                 System.out.println("codeName from database: " + codeName);
-                buttonsArray[buttonIndex+7].setText(codeName);
+                buttonsArray[buttonIndex].setText(codeName);
             } else {
                 System.out.println("No match found in the database for playerID: " + playerID);
             }
@@ -202,19 +273,21 @@ public class laser {
                 ex.printStackTrace();
             }
         } catch (NumberFormatException ex) {
-        // Handle the case where 'name' cannot be converted to an integer
+        // Handle the case where 'id' cannot be converted to an integer
         System.out.println("Invalid input. Please enter a valid integer.");
         }
     }
+
+
     public static void handleCodeName(String name, final int buttonIndex, JButton[] buttonsArray){
         String newCodeName = name;
-        String IDstring = buttonsArray[buttonIndex-7].getText();
+        String IDstring = buttonsArray[buttonIndex].getText();
         int playerID = -1;
         try{
             playerID = Integer.parseInt(IDstring);
         } catch (NumberFormatException ex) {
         // Handle the case where 'name' cannot be converted to an integer
-        System.out.println(ex);
+            System.out.println(ex);
         }
         try{
             databaseHandler.savePlayerName(playerID, newCodeName);
@@ -266,6 +339,7 @@ class DatabaseHandler{
             e.printStackTrace();
         }
     }
+    
     public String findPlayer(int playerID) throws SQLException{
         String sqlQuery = "SELECT codename FROM player WHERE id= ?";
         // String codeName = "yo";
