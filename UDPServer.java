67f3fe57cbf255901787b4acab0135579c09ee20
Regan Yates <75269018+rjyates@users.noreply.github.com>
2023-10-01 19:41:33 -0500

    Update UDPServer.java

--- UDPServer.java
+++ UDPServer.java
@@ -4,68 +4,82 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.util.ArrayList;
 
-public class UDPServer implements Runnable
-{
-	DatagramSocket ds;
+public class UDPServer implements Runnable {
+	public ArrayList<Integer> Shoot = new ArrayList<>();
+	public ArrayList<Integer> Hit = new ArrayList<>();
+	DatagramSocket rec, brd;
 
-	public void run()
-	{
-		// Step 1 : Create a socket to listen at port 1234
-		try
-		{
-			ds = new DatagramSocket(7501);
-		}
-		catch(IOException e)
-		{
+	
+	public void run() 
+    {
+		try 
+        {
+			rec = new DatagramSocket(7501);
+			brd = new DatagramSocket(7500);
+		} 
+        catch (IOException e) 
+        {
 			e.printStackTrace();
 		}
-		byte[] receive = new byte[65535];
+		
 
-		DatagramPacket DpReceive = null;
-		while (true)
-		{
+        byte[] receive = new byte[65535];
+        DatagramPacket DpReceive = null;
 
-			// Step 2 : create a DatgramPacket to receive the data.
-			DpReceive = new DatagramPacket(receive, receive.length);
+        while (true) 
+        {
+            DpReceive = new DatagramPacket(receive, receive.length);
 
-			// Step 3 : revieve the data in byte buffer.
-			try
-			{
-				ds.receive(DpReceive);
-			}
-			catch(IOException e)
-			{
-				e.printStackTrace();
-			}
+            try 
+            {
+                rec.receive(DpReceive);
+            } 
+            catch (IOException e) 
+            {
+                e.printStackTrace();
+            }
 
-			System.out.println("Client:-" + data(receive));
 
-			// Exit the server if the client sends "bye"
-			if (data(receive).toString().equals("bye"))
-			{
-				System.out.println("Client sent bye.....EXITING");
-				break;
-			}
+            System.out.println("Client:-" + data(receive));
+            String message = data(receive).toString();
+            String[] playerIds = message.split("\\:");
+            int idOfPlayerShooting = Integer.parseInt(playerIds[0]);
+            int idOfPlayerHit = Integer.parseInt(playerIds[1]);
+            Shoot.add(idOfPlayerShooting);
+            Hit.add(idOfPlayerHit);
 
-			// Clear the buffer after every message.
-			receive = new byte[65535];
-		}
-	}
+            try 
+            {
+                // Broadcast player hit
+                brd.send(
+                    new DatagramPacket(
+                        new byte[]{(byte) idOfPlayerHit},
+                        1));
+            } 
+            catch (IOException e) 
+            {
+                e.printStackTrace();
+            }	
+            receive = new byte[65535];
+        }
+    }
 
-	// A utility method to convert the byte array
-	// data into a string representation.
-	public static StringBuilder data(byte[] a)
-	{
+	public static StringBuilder data(byte[] a) 
+    {
 		if (a == null)
+        {
 			return null;
+        }
 		StringBuilder ret = new StringBuilder();
 		int i = 0;
-		while (a[i] != 0)
-		{
+		while (a[i] != 0) 
+        {
 			ret.append((char) a[i]);
 			i++;
 		}
 		return ret;
 	}
 }
+
